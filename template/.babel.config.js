c9fa1f55338b02ed0e1a9ba0ad42e04d69fa2585
Miau Lightouch <miaulightouch@gmail.com>
2018-06-09 16:43:38 +0800

    Update to babel 7

---
+++ template/.babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-env", {
+      "modules": false
+    }],
+    "@babel/preset-stage-2"
+  ],
+  plugins: ["transform-vue-jsx", "@babel/plugin-transform-runtime"]{{#if_or unit e2e}},
+  env: {
+    test: {
+      presets: ["@babel/preset-env", "@babel/preset-stage-2"]{{#if_eq runner "karma"}},
+      plugins: ["transform-vue-jsx", "istanbul"]{{/if_eq}}{{#if_eq runner "jest"}},
+      plugins: ["transform-vue-jsx", "@babel/plugin-transform-modules-commonjs", "dynamic-import-node"]{{/if_eq}}
+    }
+  }{{/if_or}}
+}
